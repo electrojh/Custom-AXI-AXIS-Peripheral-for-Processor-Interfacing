ee5e7fc0a6d2fe6bffa9c58c9899961f7a2ea958
electrojh <ccolem48@jh.edu>
2026-08-11 19:25:50 -0400

    Delete lab10_final directory

--- lab10_final/burn_sdcard.sh
+++
@@ -1,2 +0,0 @@
-sudo dd if=zybo_linux/images/linux/petalinux-sdimage.wic of=/dev/mmcblk0 status=progress 
-
--- lab10_final/flash_qspi.bat
+++
@@ -1,2 +0,0 @@
-program_flash -f vitis/my_app_system/Debug/sd_card/BOOT.BIN -fsbl vitis/hwplat/zynq_fsbl/fsbl.elf -flash_type qspi_single
-
